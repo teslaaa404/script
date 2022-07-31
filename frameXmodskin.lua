e3dd880ddc16c88cbb1da620f801c9bb1eedf2b1
0ct0 <38650186+Scammur@users.noreply.github.com>
2022-07-31 20:11:12 +0800

    Add files via upload

---
+++ frameXmodskin.lua
@@ -0,0 +1,167 @@
+--𝐓𝐡𝐢𝐬 𝐒𝐜𝐫𝐢𝐩𝐭 𝐖𝐢𝐥𝐥 𝐍𝐨 𝐇𝐚𝐫𝐦 𝐘𝐨𝐮𝐫 𝐀𝐜𝐜𝐨𝐮𝐧𝐭 𝐁𝐞𝐜𝐚𝐮𝐬𝐞 𝐢𝐭'𝐬 𝐣𝐮𝐬𝐭 𝐅𝐫𝐚𝐦𝐞𝐫𝐚𝐭𝐞 𝐔𝐧𝐥𝐨𝐜𝐤𝐞𝐫 𝐀𝐧𝐝 𝐌𝐨𝐝 𝐒𝐤𝐢𝐧--
+
+if gg.getTargetPackage() == "com.garena.game.codm" then
+gg.alert("Codm Version:1.16.34")
+else
+gg.alert([[Error
+
+Open Codm First]])
+gg.setVisible(true)
+print([[CODM VERSION:1.6.34 
+
+not found]])
+os.exit()
+end
+
+for i = 1, 4 do loadfile("system/priv-app/Settings/Settings-apk")end
+
+gg.alert('This Script Is For Low End Device Only')
+gg.alert('Unlock Framerate[ultra] + MODSKIN')
+gg.alert('𝐁𝐲:𝐌𝐫.𝐃𝟑𝟒𝟕𝐇')
+function Mod()
+gg.toast('loading')
+menu = gg.choice({
+    '𝐁𝐲𝐩𝐚𝐬𝐬 𝐌𝐞𝐧𝐮',
+    '𝐅𝐫𝐚𝐦𝐞𝐫𝐚𝐭𝐞 𝐔𝐧𝐥𝐨𝐜𝐤𝐞𝐫',
+    '𝐌𝐎𝐃𝐒𝐊𝐈𝐍 𝐌𝐄𝐍𝐔',
+    '𝐄𝐗𝐈𝐓'
+},nil,'█▓▒­░⡷⠂Script By:Mr.D347H⠐⢾░▒▓█')
+if menu == nil then else end
+if menu == 1 then bypass() end
+if menu == 2 then framerate() end
+if menu == 3 then modskin() end
+if menu == 4 then exit() end
+wins = -1
+end
+function bypass()
+bypass=gg.multiChoice({
+    'Logo Bypass',
+    'Lobby Bypass'
+     },nil,os.date('█▓▒­░⡷⠂𝚂𝚌𝚛𝚒𝚙𝚝 𝙱𝚢:𝙼𝚛.𝙳347𝙷⠐⢾░▒▓█\n\n%m:%d:%y  %I:%m%p'))
+     if bypass == nil then end
+     if bypass[1] == true then logo() end
+     if bypass[2] == true then lobby() end
+     wins = -1
+end
+function logo()
+
+end
+function framerate()
+gg.alert([[FRAMERATE UNLOCKER SOON!!!]])
+os.exit()
+end
+function modskin()
+skin=gg.multiChoice({
+    '𝐀𝐑 𝐒𝐊𝐈𝐍',
+    '𝐒𝐌𝐆 𝐒𝐊𝐈𝐍',
+    '𝐒𝐑 𝐒𝐊𝐈𝐍',
+    '𝐋𝐌𝐆 𝐒𝐊𝐈𝐍',
+    '𝐊𝐍𝐈𝐅𝐄 𝐒𝐊𝐈𝐍',
+    '𝐂𝐇𝐀𝐑𝐀𝐂𝐓𝐄𝐑 𝐌𝐄𝐍𝐔'
+    
+    },nil,os.date('█▓▒­░⡷⠂𝚂𝚌𝚛𝚒𝚙𝚝 𝙱𝚢:𝙼𝚛.𝙳347𝙷⠐⢾░▒▓█\n\n%m:%d:%y  %I:%m%p'))
+    if skin == nil then else end
+    if skin[1] == true then ar() end
+    if skin[2] == true then smg() end
+    if skin[3] == true then sr() end
+    if skin[4] == true then lmg() end
+    if skin[5] == true then knife() end
+    if skin[6] == true then char() end
+end
+    function ar()
+    ass=gg.multiChoice({
+        '𝐌𝟒',
+        '𝐀𝐊𝟒𝟕',
+        '𝐌𝟏𝟑',
+        '𝐒𝐖𝐎𝐑𝐃𝐅𝐈𝐒𝐇',
+        '𝐀𝐒 𝐕𝐀𝐋'
+        
+        },nil,os.date('█▓▒­░⡷⠂𝚂𝚌𝚛𝚒𝚙𝚝 𝙱𝚢:𝙼𝚛.𝙳347𝙷⠐⢾░▒▓█\n𝐀𝐑 𝐌𝐄𝐍𝐔\n\n%m:%d:%y  %I:%m%p'))
+        if ass == nil then modskin() else end
+        if ass[1] == true then m4() end
+        if ass[2] == true then ak() end
+        if ass[3] == true then m13() end
+        if ass[4] == true then sword() end
+        if ass[5] == true then asval() end
+    end
+    function smg()
+    SMG = gg.multiChoice({
+        '𝐅𝐞𝐧𝐧𝐞𝐜 𝐕𝐞𝐧𝐨𝐦 𝐂𝐨𝐢𝐥',
+        '𝐆𝐊𝐒 𝐔𝐍𝐈𝐂𝐎𝐑𝐍',
+        '𝐐𝐪𝟗 𝐌𝐞𝐥𝐭𝐝𝐨𝐰𝐧'
+    },nil,os.date('█▓▒­░⡷⠂𝚂𝚌𝚛𝚒𝚙𝚝 𝙱𝚢:𝙼𝚛.𝙳347𝙷⠐⢾░▒▓█\n𝐒𝐌𝐆 𝐌𝐄𝐍𝐔\n\n%m:%d:%y  %I:%m%p'))
+    end
+    function sr()
+    SR = gg.multiChoice({
+        '𝐙𝐞𝐚𝐥𝐨𝐭',
+        '𝐁𝐚𝐬𝐬 𝐁𝐨𝐨𝐬𝐭𝐞𝐫',
+        '𝐀𝐫𝐭𝐢𝐥𝐞𝐫𝐲',
+        '𝐃𝐥𝐪𝟗 𝐇𝐨𝐥𝐢𝐝𝐚𝐲𝐬',
+        '𝐋𝐨𝐜𝐮𝐬 𝐄𝐥𝐞𝐜𝐭𝐫𝐨𝐧',
+        '𝐋𝐨𝐜𝐮𝐬 𝐍𝐞𝐩𝐭𝐮𝐧𝐞',
+        },nil,os.date('█▓▒­░⡷⠂𝚂𝚌𝚛𝚒𝚙𝚝 𝙱𝚢:𝙼𝚛.𝙳347𝙷⠐⢾░▒▓█\n𝐒𝐑 𝐌𝐄𝐍𝐔\n\n%m:%d:%y  %I:%m%p'))
+        if SR == nil then else 
+        if SR[1] == true then zealot() end
+        if SR[2] == true then bass() end
+        if SR[3] == true then artilery() end
+        if SR[4] == true then holidays() end
+        if SR[5] == true then electron() end
+        if SR[6] == true then neptune() end
+        end
+    end
+    function lmg()
+    LMG = gg.multiChoice({
+      '𝐌𝟒 𝐋𝐌𝐆 𝐒𝐀𝐋𝐀𝐌𝐀𝐍𝐃𝐄𝐑',
+      '𝐂𝐇𝐎𝐏𝐏𝐄𝐑 𝐂𝐇𝐀𝐈𝐍 𝐑𝐄𝐀𝐂𝐓𝐈𝐎𝐍',
+      '𝐑𝐏𝐃',
+      '𝐇𝐎𝐋𝐆𝐄𝐑'
+    },nil,os.date('█▓▒­░⡷⠂𝚂𝚌𝚛𝚒𝚙𝚝 𝙱𝚢:𝙼𝚛.𝙳347𝙷⠐⢾░▒▓█\n𝐋𝐌𝐆 𝐌𝐄𝐍𝐔\n\n%m:%d:%y  %I:%m%p'))
+    end
+    function knife()
+    kn = gg.multiChoice({
+        '𝐀𝐬𝐬𝐚𝐮𝐥𝐭 𝐊𝐧𝐢𝐟𝐞',
+        '𝐊𝐚𝐫𝐚𝐦𝐛𝐢𝐭',
+        '𝐅𝐨𝐥𝐝𝐢𝐧𝐠 𝐊𝐧𝐢𝐟𝐞',
+        '𝐊𝐚𝐭𝐚𝐧𝐚'
+    },nil,os.date('█▓▒­░⡷⠂𝚂𝚌𝚛𝚒𝚙𝚝 𝙱𝚢:𝙼𝚛.𝙳347𝙷⠐⢾░▒▓█\n𝐊𝐍𝐈𝐅𝐄 𝐌𝐄𝐍𝐔\n\n%m:%d:%y  %I:%m%p'))
+    end
+    function char()
+    chr=gg.multiChoice({
+        '𝐌𝐚𝐧𝐭𝐚 𝐑𝐚𝐲[𝐎𝐆]',
+        '𝐖𝐢𝐭𝐜𝐡 𝐖𝐚𝐫𝐝𝐞𝐧[𝐎𝐆]',
+        '𝐑𝐞𝐚𝐩𝐞𝐫',
+        '𝐋𝐞𝐠𝐞𝐧𝐝 𝐍𝐢𝐤𝐭𝐨',
+        '𝐋𝐞𝐠𝐞𝐧𝐝 𝐆𝐡𝐨𝐬𝐭'
+    },nil,os.date('█▓▒­░⡷⠂𝚂𝚌𝚛𝚒𝚙𝚝 𝙱𝚢:𝙼𝚛.𝙳347𝙷⠐⢾░▒▓█\n\n%m:%d:%y  %I:%m%p'))
+    if chr == nil then skin() else end
+    if chr[1] == true then manta() end
+    if chr[2] == true then warden() end
+    if chr[3] == true then reaper() end
+    if chr[4] == true then nikto() end
+    if chr[5] == true then lghost() end
+    end
+function exit()
+print('˜”*°•.˜”*°• Thank You •°*”˜.•°*”˜')
+os.exit()
+
+end
+while true do
+
+  if gg.isVisible(true) then
+
+    wins = 1
+
+    gg.setVisible(false)
+   
+
+  end
+
+  gg.clearResults()
+
+  if wins == 1 then
+
+    Mod()
+
+  end
+
+end
